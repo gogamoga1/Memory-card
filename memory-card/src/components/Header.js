1a9f2192f4362df89f650bdec3ba4e001a7187ab
Pavel Milyaev <verccety1@gmail.com>
2020-11-14 16:23:27 +0700

    Added logo

--- memory-card/src/components/Header.js
+++ memory-card/src/components/Header.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import logo from '../logo.png'; // Tell webpack this JS file uses this image
 
-const Header = ({curScore, bestScore}) => {
+
+const Header = ({ curScore, bestScore }) => {
   return (
     <header>
-            <div className='project-name'>MEMORY GAME</div>
+      <div className="project-name">
+        {' '}
+        <a href="https://github.com/gogamoga1/Memory-card/tree/master/memory-card">
+          <img id="github" src={logo} alt="github" /> Memory Game
+        </a>
+      </div>
 
       <div className="header-score-container">
         <p>Best Score: {bestScore}</p>
-        <hr style={{width: '100%'}} />
+        <hr style={{ width: '100%' }} />
         <p>Current Score: {curScore} </p>
       </div>
     </header>
